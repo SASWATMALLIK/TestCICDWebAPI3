619e418f89ddb4b8efea51b42808bff67d3f97ff
saswat-mallik <saswat.mallik96@gmail.com>
2026-09-17 21:22:40 +0530

    changes

--- TestCICDWebAPI/Controllers/WeatherForecastController.cs
+++ TestCICDWebAPI/Controllers/WeatherForecastController.cs
@@ -25,15 +25,11 @@ namespace TestCICDWebAPI.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Name ="Saswat"
             })
             .ToArray();
         }
 
-        [HttpGet(Name = "GetMyName")]
-        public string GetMyName()
-        {
-            return "Saswat";
-        }
     }
 }
